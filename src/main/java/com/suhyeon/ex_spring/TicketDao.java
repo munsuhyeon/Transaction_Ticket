36393c51110aec68bb8267b2bad83cf236c8e213
hrno3826@naver.com <msh1126psh>
2022-04-28 09:51:23 +0900

    Transaction 완성

--- src/main/java/com/suhyeon/ex_spring/TicketDao.java
+++ src/main/java/com/suhyeon/ex_spring/TicketDao.java
@@ -7,26 +7,25 @@ import java.sql.SQLException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
 public class TicketDao {
+	
 	JdbcTemplate template;
-	TransactionTemplate transactionTemplate;
+	
 	PlatformTransactionManager transactionManager;
 	
 	
-	public void setTransactionManager(PlatformTransactionManager transactionManager) {
-		this.transactionManager = transactionManager;
-	}
-
 	public void setTemplate(JdbcTemplate template) {
 		this.template = template;
 	}
 
-	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
-		this.transactionTemplate = transactionTemplate;
+	public void setTransactionManager(PlatformTransactionManager transactionManager) {
+		this.transactionManager = transactionManager;
 	}
 
 	public TicketDao() {
@@ -36,55 +35,53 @@ public class TicketDao {
 	
 	public void buyTicket(final TicketDto dto) {
 		
-		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+		TransactionDefinition definition = new DefaultTransactionDefinition();
+		
+		TransactionStatus status = transactionManager.getTransaction(definition);
+		
+		try {
+			template.update(new PreparedStatementCreator() {
+				
+				//카드결제
+				@Override
+				public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
 			
-			@Override
-			protected void doInTransactionWithoutResult(TransactionStatus status) {
-				
-				try {
-				template.update(new PreparedStatementCreator() {
+					String query="INSERT INTO card (consumerid, amount) VALUES (?, ?)";
+					PreparedStatement pstmt = con.prepareStatement(query);
+					pstmt.setString(1,dto.getConsumerid());
+					pstmt.setInt(2,dto.getAmount());
 					
-					//카드결제
-					@Override
-					public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-				
-						String query="INSERT INTO card (consumerid, amount) VALUES (?, ?)";
-						PreparedStatement pstmt = con.prepareStatement(query);
-						pstmt.setString(1,dto.getConsumerid());
-						pstmt.setInt(2,dto.getAmount());
-						
-						return pstmt;
-					}
-				});
-				
-				template.update(new PreparedStatementCreator() {
-					
-					//티켓구매
-					@Override
-					public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-				
-						String query="INSERT INTO ticket (consumerid, countnum) VALUES (?, ?)";
-						PreparedStatement pstmt = con.prepareStatement(query);
-						pstmt.setString(1,dto.getConsumerid());
-						pstmt.setInt(2,dto.getAmount());
-						return pstmt;
-					}
-				});
-				
-				
-				
-				}catch(Exception e) {
-					e.printStackTrace();
-					
-					HomeController cont = new HomeController();
-					
-					cont.countError();
-					
-					System.out.println("Rollback!!");
-					
-					
+					return pstmt;
 				}
+			});
+			
+			
+			template.update(new PreparedStatementCreator() {
+				
+				//티켓구매
+				@Override
+				public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+			
+					String query="INSERT INTO ticket (consumerid, countnum) VALUES (?, ?)";
+					PreparedStatement pstmt = con.prepareStatement(query);
+					pstmt.setString(1,dto.getConsumerid());
+					pstmt.setInt(2,dto.getAmount());
+					return pstmt;
+				}
+			});
+			
+			transactionManager.commit(status);
+			
+			}catch(Exception e) {
+				e.printStackTrace();
+				
+				HomeController cont = new HomeController();
+				
+				cont.countError();
+				
+				System.out.println("Rollback!!");	
+				
+				transactionManager.rollback(status);
 			}
-		});
+		}
 	}
-}
